1aab57da584d7171a03ded58168937d7f67ca4fb
halak0013 <zumruduanka0013@gmail.com>
2025-07-02 19:55:21 +0300

    added missing var folder

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 mkdir -p base-files
 cp -rf etc DEBIAN usr base-files
 cd base-files
-mkdir bin  boot  dev  etc  home  lib  proc  root  run  sbin  sys  tmp  var
+mkdir bin  boot  dev  etc  home  lib  proc  root  run  sbin  sys  tmp
+mkdir -p var/{backups,cache,lib/{dpkg,misc},local,lock,log,run,spool,tmp}
 cd ..
 python3 base_file_pack_unpack.py
